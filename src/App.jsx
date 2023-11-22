eb76124a3aecaea74d4fcb574c3437ef2f23836c
prinsaa <prinsaptl@outlook.com>
2023-11-21 21:31:15 -0500

    fixed up the CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,158 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const list = [
+  {
+    front: "Start",
+    back: "Press the Next Card button to start! :)",
+  },
+  {
+    front: "🕷️👨",
+    back: "Spiderman",
+    difficulty: "easy",
+  },
+  {
+    front: "💑🚢🥶",
+    back: "Titanic",
+    difficulty: "easy",
+  },
+  {
+    front: "🌊1️⃣1️⃣",
+    back: "Ocean's 11",
+    difficulty: "medium",
+  },
+  {
+    front: "🦖🦕🏞️",
+    back: "Jurassic Park",
+    difficulty: "easy",
+  },
+  {
+    front: "📈🤑👔🧑🏻‍💼",
+    back: "The Wolf of Wall Street",
+    difficulty: "medium",
+  },
+  {
+    front: "🌌💫🧑‍🚀👨‍🚀👩‍",
+    back: "Interstellar",
+    difficulty: "hard",
+  },
+  {
+    front: "❤️🥇🛍️🏫🧨🙅🏻‍♀️❤️",
+    back: "The Breakfast Club",
+    difficulty: "hard",
+  },
+  {
+    front: "🧹👸💃🕛👠",
+    back: "Cinderella",
+    difficulty: "easy",
+  },
+  {
+    front: "🔟💑😡👎",
+    back: "10 Things I Hate About You",
+    difficulty: "hard",
+  },
+  {
+    front: "🦁👑🐒🐗",
+    back: "The Lion King",
+    difficulty: "medium",
+  },
+];
+
+const App = () => {
+  const [currentIndex, setIndex] = useState(0);
+  const [showFront, setToFront] = useState(true);
+  const [userGuess, setUserGuess] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [inputClassName, setInputClassName] = useState("input-box");
+  const [score, setScore] = useState(0);
+
+  const currentCard = list[currentIndex];
+
+  const updateQuestion = (direction) => {
+    if (direction == "forward" && currentIndex < list.length - 1) {
+      setIndex(currentIndex + 1);
+    } else if (direction == "back" && currentIndex > 0) {
+      setIndex(currentIndex - 1);
+    }
+
+    setToFront(true);
+    setUserGuess("");
+    setFeedback("");
+    setInputClassName("input-box");
+  };
+
+  const showAnswer = () => {
+    if (userGuess.toLowerCase() == currentCard.back.toLowerCase()) {
+      setFeedback("Correct!");
+      setScore(score + 1);
+      setInputClassName("input-box correct-answer");
+    } else {
+      setFeedback("Incorrect. Try again!");
+      setInputClassName("input-box incorrect-answer");
+    }
+
+    setToFront(!showFront);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h2 style={{ color: "#d62929" }}>Filmoji Quiz</h2>
+      <h3 style={{ fontSize: "25px" }}>
+        Wanna test your wit? Translate the emojis to a Hollywood movie! The
+        background colour of each card indicates the difficulty level.
+      </h3>
+      <h4 style={{ fontSize: "20px" }}>
+        There are 10 cards in total to guess from! GOOD LUCK!{" "}
+      </h4>
+      <div className="container" onClick={showAnswer}>
+        <div
+          className={`card ${showFront ? "" : "card-flip"} ${
+            currentCard.difficulty
+          }`}
+        >
+          {showFront ? (
+            <div className="question">
+              <h1>{currentCard.front}</h1>
+            </div>
+          ) : (
+            <div className="answer">
+              <h2 className="back">{currentCard.back}</h2>
+            </div>
+          )}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      {currentIndex !== 0 && ( // Show the user input section only when not on the first card
+        <div>
+          <input
+            type="text"
+            placeholder="Enter your answer"
+            value={userGuess}
+            onChange={(e) => setUserGuess(e.target.value)}
+            className={inputClassName}
+          />
+          <button onClick={showAnswer}>Submit</button>
+          <p>{feedback}</p>
+        </div>
+      )}
 
-export default App
+      <button
+        style={{ marginRight: "10px" }}
+        onClick={() => updateQuestion("back")}
+        disabled={currentIndex === 0}
+      >
+        Previous
+      </button>
+
+      <button
+        onClick={() => updateQuestion("forward")}
+        disabled={currentIndex === list.length - 1}
+      >
+        Next
+      </button>
+
+      <h2>Here's your score: {score}</h2>
+    </div>
+  );
+};
+
+export default App;
